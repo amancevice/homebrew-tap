0e029cbe8ef9151877f1b044adfcd6c219a810e0
Alexander Mancevice <alexander.mancevice@hey.com>
2021-03-11 17:25:20 -0500

    fix for brewcleaner formula

--- Formula/brewcleaner.rb
+++ Formula/brewcleaner.rb
@@ -6,27 +6,17 @@ class Brewcleaner < Formula
   url "https://registry.npmjs.org/brewcleaner/-/brewcleaner-1.1.0.tgz"
   sha256 "c43965aef7066fba349f5641db3449e7c8d36a54dc19c12874f82214162471d8"
 
-  bottle do
-    root_url "https://github.com/amancevice/homebrew-tap/releases/download/brewcleaner-1.1.0"
-    cellar :any
-    sha256 "49af8173ec384827ab72de9dc67e230e2577dcc44f60d60b5574f51db4530598" => :high_sierra
-    sha256 "54384e990b0eee838e566eb901e461e5fefa66583a36be9ec3aa5c98d598883a" => :sierra
-  end
-
   depends_on "node"
 
   def install
-    # Install brewcleaner
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
-
-    # Install whitelist
-    (buildpath/"brew_whitelist.json").write "{}"
-    (etc/"brewcleaner").install "brew_whitelist.json"
+    (bin/"brewcleaner").write <<~SH
+      #!/bin/bash
+      #{libexec/"bin/brewcleaner"} $@ $BREWCLEANER_WHITELIST
+    SH
   end
 
   test do
-    ENV["LC_ALL"] = "en_US.UTF-8"
-    assert_match "Finished", shell_output("#{libexec}/lib/node_modules/brewcleaner/bin/brewcleaner run")
+    assert_match "Finished", shell_output("brewcleaner run")
   end
 end
