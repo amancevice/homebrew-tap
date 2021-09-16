9f8df8be253579dee3293c3f60ffebd83e347627
Alexander Mancevice <alexander.mancevice@hey.com>
2021-09-16 12:53:29 -0400

    Add rainbow-sh formula

---
+++ Formula/rainbow-sh.rb
@@ -0,0 +1,14 @@
+class RainbowSh < Formula
+  desc "Print the rainbow"
+  homepage "TODO"
+  url "https://gist.github.com/amancevice/eef0837944f85bc7c8e64c37209a202f/archive/f593e00ccc92f4b07e711e6bdb2c259b2d5864d3.zip"
+  version "1.0"
+  sha256 "3515ad04ff486cdbe0d18ebaf0d2166af21251830372033a9ca060ab7ef0483f"
+
+  def install
+    bin.install "rainbow"
+  end
+
+  test do
+  end
+end
