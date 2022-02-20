f9a2e42a05252515030c1101b99ca34f9c8f5861
Alexander Mancevice <a.mancevice@kickstarter.com>
2022-02-20 12:47:58 -0500

    shenv ~> 0.1.1

--- Formula/shenv.rb
+++ Formula/shenv.rb
@@ -1,8 +1,8 @@
 class Shenv < Formula
   desc "Source .env files into your shell"
   homepage "https://github.com/amancevice/shenv"
-  url "https://github.com/amancevice/shenv/archive/0.1.0.tar.gz"
-  sha256 "682542847b0357bf372b195292d9a687b126fa5311b5ff7dab38c259072fa9b5"
+  url "https://github.com/amancevice/shenv/archive/0.1.1.tar.gz"
+  sha256 "4252c948f814ae810cf2093d3905a24835a21a9cd04c1bae5530106618c7948e"
 
   def install
     libexec.install "bin"
